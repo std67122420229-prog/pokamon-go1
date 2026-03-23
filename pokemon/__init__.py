54f064f548122d99a6c31a15c3ccbf39f85f2eb6
std67122420229-prog <std.67122420229@ubru.ac.th>
2026-03-23 21:59:42 +0700

    Update __init__.py

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -8,20 +8,26 @@ from pokemon.pokemon.routes import pokemon_bp
 
 def create_app():
   app = Flask(__name__)
-  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-  with app.app_context():
-    db.create_all()
 
+  # กันพังถ้าไม่มี DATABASE_URL
+  app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///app.db'
+  app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'secret123'
+
+  # ✅ ต้อง init ก่อน
   db.init_app(app)
   bcrypt.init_app(app)
   login_manager.init_app(app)
+
   login_manager.login_view = 'users.login'
   login_manager.login_message = 'Please login before access this page!'
   login_manager.login_message_category = 'warning'
 
+  # ✅ แล้วค่อย create db
+  with app.app_context():
+    db.create_all()
+
   app.register_blueprint(core_bp, url_prefix='/')
   app.register_blueprint(users_bp, url_prefix='/users')
   app.register_blueprint(pokemon_bp, url_prefix='/pokemons')
-  
+
   return app
